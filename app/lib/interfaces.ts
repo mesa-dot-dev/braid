116d1d8e1e81a7c8e432ab8d19bdec15d37101ac
Oliver Gilan <github@olivergilan.com>
2024-12-09 11:38:03 -0800

    github first pass

--- app/lib/interfaces.ts
+++ app/lib/interfaces.ts
@@ -1,17 +1,35 @@
 import { StatusMessageTable } from "@/database/status-message";
-import { createSelectSchema } from "drizzle-zod";
 
-export interface Product {
+export type RSSFeed = {
+  title: string;
+  link: string;
+  description: string;
+  pubDate: string;
+};
+
+export interface IProduct {
   name: string;
   displayName: string;
   logo: string;
-  getServices: () => Promise<Product[]>;
+  getServices: () => Promise<IService[]>;
   getFeed: () => Promise<StatusMessage[]>;
+  classifyMessage: (message: StatusMessage) => Promise<ClassifiedMessage>;
 }
 
-// export const StatusMessageSchema = createSelectSchema(StatusMessageTable).omit({
-//   product: true,
-//   affectedServices: true,
-// });
+export interface IService {
+  name: string;
+  displayName: string;
+}
+
 export type StatusMessage = Omit<typeof StatusMessageTable.$inferSelect, "product" | "affectedServices">;
 export type ClassifiedMessage = typeof StatusMessageTable.$inferSelect;
+
+export abstract class Product implements IProduct {
+  abstract readonly name: string;
+  abstract readonly displayName: string;
+  abstract readonly logo: string;
+
+  abstract getServices(): Promise<IService[]>;
+  abstract getFeed(): Promise<StatusMessage[]>;
+  abstract classifyMessage(message: StatusMessage): Promise<ClassifiedMessage>;
+}
