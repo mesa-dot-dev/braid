328145fb03ee02c38d545f006cf576a5140c206f
Oliver Gilan <github@olivergilan.com>
2024-12-09 12:05:47 -0800

    cron job

---
+++ app/functions/run.ts
@@ -0,0 +1,11 @@
+export async function handler() {
+  console.log(`[${new Date().toISOString()}] Running Status Check`);
+
+  // Fetch new messages from all services
+
+  // Classify messages
+
+  // Store messages in database
+
+  // Notify relevant slack apps
+}
